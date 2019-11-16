aab29554111ceb310199b5cfe7b3f64706b3de63
JC Lenz <jc@miserablepop.com>
2019-11-16 13:40:26 -0800

    Added a test reservation object

--- app/data/reservation.js
+++ app/data/reservation.js
@@ -2,4 +2,11 @@
 var waitlist = [];
 
 // Reservation Data
-var reservations = [];
+var reservations = [
+    {
+        name: 'Test Foo',
+        phoneNum: '206-123-4567',
+        email: 'test@email.com',
+        uniqueID: '123456'
+    }
+];
